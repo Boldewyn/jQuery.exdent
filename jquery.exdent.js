a8dead31ce842c9a069f75e322614dc1d3a98a3b
Manuel Strehl <git@manuel-strehl.de>
2011-11-22 07:44:45 +0100

    fix case, where exdenting moves element in previous line

--- jquery.exdent.js
+++ jquery.exdent.js
@@ -44,7 +44,7 @@
     return this.each(function() {
 
       var $this = $(this), left = 0,
-          width, tmp, cacheKey;
+          width, tmp, cacheKey, top;
 
       // we must detect the desired width upfront, because we need it
       // esp. in case of :before pseudo-elements
@@ -75,8 +75,13 @@
       tmp.remove();
       tmp = null;
 
+      $this.prev('br._exdent_helper').remove();
       if (left - width - 1 <= $this.parent().offset().left) {
+        top = $this.offset().top;
         $this.css('marginLeft', -width).addClass(o.className);
+        if (top !== $this.offset().top) {
+          $this.before('<br class="_exdent_helper"/>');
+        }
       } else {
         $this.css('marginLeft', '0').removeClass(o.className);
       }
